3ce2cb1517e31d74a81e8aa1567cc76c9b7b8d40
Chris Jones <chris@brack3t.com>
2013-09-27 18:14:07 -0700

    stub home view.

--- headcount/events/urls.py
+++ headcount/events/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 
-from .views import Dashboard
+from .views import Dashboard, Home
 
 
 urlpatterns = patterns(
     '',
+    url(r'^$', Home.as_view(), name='home'),
     url(r'^dashboard/$', Dashboard.as_view(), name='dashboard'),
 )
--- headcount/events/views.py
+++ headcount/events/views.py
@@ -4,6 +4,11 @@ from django.views.generic import View
 from braces.views import LoginRequiredMixin
 
 
+class Home(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('HOME!')
+
+
 class Dashboard(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("HOLA")
+        return HttpResponse('DASHBOARD!')
