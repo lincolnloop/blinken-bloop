452c0308d8c85df647c640cd69d5feb3dc48fb33
Chris Jones <chris@brack3t.com>
2013-09-27 17:24:22 -0700

    PEP8.

--- headcount/headcount/urls.py
+++ headcount/headcount/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'headcount.views.home', name='home'),
-    # url(r'^headcount/', include('headcount.foo.urls')),
 
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
 )
