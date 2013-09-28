33c7b965600134249138747e8c23af8d77e28a4c
Kenneth Love <kenneth@gigantuan.net>
2013-09-27 18:08:38 -0700

    ready for tests

--- headcount/headcount/settings/base.py
+++ headcount/headcount/settings/base.py
@@ -120,6 +120,7 @@ DJANGO_APPS = (
 THIRD_PARTY_APPS = (
     'south',
     'authtools',
+    'discover_runner',
 )
 LOCAL_APPS = (
     'events',
@@ -158,3 +159,8 @@ LOGGING = {
         },
     }
 }
+
+TEST_RUNNER = 'discover_runner.DiscoverRunner'
+TEST_DISCOVER_TOP_LEVEL = root()
+TEST_DISCOVER_ROOT = root()
+TEST_DISCOVER_PATTERN = 'test_*'
