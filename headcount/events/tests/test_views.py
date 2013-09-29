9620a5eb4205a8c3a3669f88dd2af07e9ef6534c
Chris Jones <chris@brack3t.com>
2013-09-29 13:19:12 -0700

    test user login works.

--- headcount/events/tests/test_views.py
+++ headcount/events/tests/test_views.py
@@ -8,14 +8,19 @@ from .test_forms import create_event
 class BaseTestCase(TestCase):
     def setUp(self):
         self.user = self._create_user()
-        self.event = create_event()
+        self.event = create_event(self.user)
 
-    def _create_user(self, username=u'test@example.com'):
-        return User.objects.create(
-            username=username,
-            password='test123'
+    def _create_user(self, email=u'test@example.com'):
+        return User.objects.create_user(
+            name='Tester McTesterson',
+            email=email,
+            password='test123',
+            is_active=True
         )
 
-    def _login_user(self, username='test@example.com', password='test123'):
+    def _login_user(self, email=u'test@example.com', password=u'test123'):
         self.assertTrue(
-            self.client.login(username=username, password=password))
+            self.client.login(email=email, password=password))
+
+    def test_home_page(self):
+        self._login_user()
