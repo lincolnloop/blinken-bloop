20b7c58b604b7a3fb0ff6929cb88d9852b5a0bf8
Kenneth Love <kenneth@gigantuan.net>
2013-09-28 18:38:58 -0700

    email the host when someone RSVPs yes

--- headcount/events/signals.py
+++ headcount/events/signals.py
@@ -46,7 +46,7 @@ def event_delete(sender, instance, **kwargs):
         },
         to=to_emails
     )
-    email.send(fail_silently=False)
+    email.send(fail_silently=True)
 
 
 def rsvp_creation(sender, instance, created, raw, **kwargs):
@@ -75,4 +75,18 @@ def rsvp_delete(sender, instance, **kwargs):
         },
         to=(to_email,)
     )
-    email.send(fail_silently=False)
+    email.send(fail_silently=True)
+
+
+def rsvp_notification(sender, instance, created, raw, **kwargs):
+    if instance.response == instance.RESPONSE_CHOICES.yes:
+        to_email = '{0.name} <{0.email}>'.format(instance.event.host)
+        email = generate_email(
+            'events/email/notify_rsvp',
+            HttpRequest(),
+            {
+                'instance': instance,
+            },
+            to=(to_email,)
+        )
+        email.send(fail_silently=True)
