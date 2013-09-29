f8f4ca1b74fe73ff75298686bce5d5c4ed953aaf
Chris Jones <chris@brack3t.com>
2013-09-29 13:10:20 -0700

    better create user.

--- headcount/events/tests/test_views.py
+++ headcount/events/tests/test_views.py
@@ -1,13 +1,20 @@
 from django.test import TestCase
 
+from authtools.models import User
+
 from .test_forms import create_event
-from .test_models import create_user
 
 
 class BaseTestCase(TestCase):
     def setUp(self):
-        self.user = create_user()
+        self.user = self._create_user()
         self.event = create_event()
+
+    def _create_user(self, username=u'test@example.com'):
+        return User.objects.create(
+            username=username,
+            password='test123'
+        )
 
     def _login_user(self, username='', password=''):
         self.assertTrue(
