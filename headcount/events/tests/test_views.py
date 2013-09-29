cdc35e6df136b4249ae29b9e72f95dd1fe3bee56
Chris Jones <chris@brack3t.com>
2013-09-29 13:11:05 -0700

    tests.

--- headcount/events/tests/test_views.py
+++ headcount/events/tests/test_views.py
@@ -16,14 +16,6 @@ class BaseTestCase(TestCase):
             password='test123'
         )
 
-    def _login_user(self, username='', password=''):
+    def _login_user(self, username='test@example.com', password='test123'):
         self.assertTrue(
             self.client.login(username=username, password=password))
-
-    def _set_site_session_data(self, session, site):
-        session['current_site'] = {
-            'pk': site.pk,
-            'name': site.name,
-            'domain': site.domain
-        }
-        session.save()
