6de1e76c205e309a2a61fdbef19ffb8daae3da7b
Karen Rustad <karen.rustad@gmail.com>
2013-09-29 12:30:07 -0700

    clever anchor link w. notes showing from the dashboard

--- headcount/static/js/rsvp-details.js
+++ headcount/static/js/rsvp-details.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
     $('.notes-all').hide();
 
+    console.log(window.location.hash);
+
     $('.rsvp-table').on('click', '.notes-truncated', function(e){
         e.preventDefault();
         $('#notes-' + $(this).parents('tr').attr('id').slice(5)).toggle(300);
     });
+
+    if (window.location.hash) {
+        $(window.location.hash).addClass('highlight');
+        $('#notes-' + window.location.hash.slice(6)).addClass('highlight').toggle();
+    }
 });
