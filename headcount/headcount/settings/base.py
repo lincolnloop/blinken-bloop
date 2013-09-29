c45d4868a9ec8eba59638f00e30a35a70e9fae23
Kenneth Love <kenneth@gigantuan.net>
2013-09-29 13:31:31 -0700

    added headcount.io to allowed domains

--- headcount/headcount/settings/base.py
+++ headcount/headcount/settings/base.py
@@ -21,7 +21,7 @@ LOGIN_REDIRECT_URL = '/dashboard/'
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['headcountio.herokuapp.com']
+ALLOWED_HOSTS = ['headcountio.herokuapp.com', 'headcount.io']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
